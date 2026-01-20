4ad16dd5e8526ac5a7bc6b3acb1d19fa15362829
vent171717-crypto <vent171717@gmail.com>
2026-01-20 14:23:35 -0400

    Delete deepseek_swift_20260120_4ec896.swift

--- deepseek_swift_20260120_4ec896.swift
+++
@@ -1,8 +0,0 @@
-import AdSupport
-import CoreLocation
-
-let advertisingId = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-
-// Usar CLLocationManager para señales
-let locationManager = CLLocationManager()
-// Configurar y recoger datos de entorno
